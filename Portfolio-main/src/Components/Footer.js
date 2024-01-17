eb45fee1b688c8a7543ae984735e4a5d535472fc
JeevaS43 <jeevas21@karunya.edu.in>
2024-01-17 22:53:25 +0530

    a

--- Portfolio-main/src/Components/Footer.js
+++ Portfolio-main/src/Components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
       <h4>Copyright &copy; 2023 JS</h4>
       <div className='footerLinks'>
         <a href="https://github.com/JeevaS43" target='_blank'><FaGithub/></a>
-        <a href="hhttps://www.linkedin.com/in/jeeva-s-039a11226/" target='_blank'><FaLinkedin/></a>
+        <a href="https://www.linkedin.com/in/jeeva-s-039a11226/" target='_blank'><FaLinkedin/></a>
         <a href='mailTo:jeevasofficial43@gmail.com' target='_blank'><GrMail/></a>
         {/* <a href="https://leetcode.com/sahni1403/" target="_blank"><SiLeetcode/></a> */}
       </div>
